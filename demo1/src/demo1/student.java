04f1981f321195b37bb01ac58109222d21ae18f9
ranjitha-narayana <90611976+ranjitha-narayana@users.noreply.github.com>
2022-01-10 11:53:09 +0530

    updated commit

--- demo1/src/demo1/student.java
+++ demo1/src/demo1/student.java
@@ -4,6 +4,7 @@ public class student {
 	public static void main(String[] args) {
 		System.out.println("hello");
 		System.out.println("from master");
+		System.out.println("hi ranjitha");
 		
 	}
 
